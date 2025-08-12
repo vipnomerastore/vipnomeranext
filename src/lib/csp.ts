22f71e6db01e6c0adf1dfbfd27f19de6476d6697
Orkhan Rahimli <orduxan144@gmail.com>
2025-08-12 11:05:21 +0300

    fix

--- src/lib/csp.ts
+++ src/lib/csp.ts
@@ -1,16 +1,15 @@
 const cspHeader = `
     default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://mc.yandex.ru https://mc.yandex.com https://app.daily-grow.com;
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://mc.yandex.ru https://mc.yandex.com https://app.daily-grow.com https://app.reviewlab.ru https://api-maps.yandex.ru;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data: https: https://mc.yandex.ru https://mc.yandex.com;
-    font-src 'self';
-    connect-src 'self' https://mc.yandex.ru https://mc.yandex.com https://api.daily-grow.com;
+    font-src 'self' data:;
+    connect-src 'self' https://mc.yandex.ru https://mc.yandex.com https://app.daily-grow.com https://api.daily-grow.com;
     frame-src 'self' https://mc.yandex.com https://mc.yandex.ru;
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
-    upgrade-insecure-requests;
 `;
 
 export { cspHeader };
