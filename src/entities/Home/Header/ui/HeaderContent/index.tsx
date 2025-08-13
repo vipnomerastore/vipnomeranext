d206de46e562399a30fd7a186d5bf2633cf5976c
admin <matros22091994@gmail.com>
2025-08-13 13:24:02 +0500

    blog

--- src/entities/Home/Header/ui/HeaderContent/index.tsx
+++ src/entities/Home/Header/ui/HeaderContent/index.tsx
@@ -10,22 +10,27 @@ interface HeaderContentProps {
 const HeaderContent = ({ openModal }: HeaderContentProps) => {
   return (
     <div className={styles.content}>
+      <div className={styles.eliteBadge}>
+        <Image
+          src="/assets/home/numbers/crown.svg"
+          alt="Корона"
+          width={24}
+          height={24}
+        />
+        <span className={styles.eliteText}>Элитные</span>
+      </div>
       <h1 className={styles.mainTitle}>
-        <div className={styles.eliteBadge}>
-          <Image
-            src="/assets/home/numbers/crown.svg"
-            alt="Корона"
-            width={24}
-            height={24}
-          />
-          <span className={styles.eliteText}>Элитные</span>
-        </div>
         НОМЕРА <br /> ТЕЛЕФОНОВ
-        <p className={styles.description}>
-          Большой выбор предложений по лучшим ценам <br /> Красивые Номера с
-          официальным оформлением
-        </p>
+        <br />
+        <span className={styles.subtitleMain}>
+          Большой выбор Красивых, Запоминающихся, Выгодных номеров
+        </span>
       </h1>
+
+      <p className={styles.description}>
+        Предложения по лучшим ценам <br /> Красивые Номера с официальным
+        оформлением
+      </p>
 
       <Button onClick={openModal}>Получить Красивый номер от 2 499 р.</Button>
     </div>
