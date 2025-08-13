9826990e6c5b6a3a4cfe2e6dc7e5b8bc73c83c6c
admin <matros22091994@gmail.com>
2025-08-13 18:51:22 +0500

    fix

--- src/entities/Home/Header/index.tsx
+++ src/entities/Home/Header/index.tsx
@@ -37,52 +37,6 @@ const HomeHeader = () => {
         <div className={styles.container}>
           <HeaderContent openModal={openModal} />
 
-          <div className={styles.bannersContainer}>
-            <div className={styles.banner}>
-              <Image
-                src="/og-image.jpg"
-                alt="VIP номера со скидками"
-                className={styles.bannerImage}
-                priority
-                fill
-                sizes="(max-width: 768px) 100vw, 200px"
-                style={{ objectFit: "cover" }}
-              />
-              <div className={styles.bannerOverlay}>
-                <h3 className={styles.bannerTitle}>Скидки до 90%</h3>
-                <p className={styles.bannerText}>На все VIP номера</p>
-              </div>
-            </div>
-            <div className={styles.banner}>
-              <Image
-                src="/og-image.jpg"
-                alt="Лучшие телефонные номера"
-                className={styles.bannerImage}
-                fill
-                sizes="(max-width: 768px) 100vw, 200px"
-                style={{ objectFit: "cover" }}
-              />
-              <div className={styles.bannerOverlay}>
-                <h3 className={styles.bannerTitle}>Премиум номера</h3>
-                <p className={styles.bannerText}>Эксклюзивная коллекция</p>
-              </div>
-            </div>
-            <div className={styles.banner}>
-              <Image
-                src="/og-image.jpg"
-                alt="Быстрая доставка номеров"
-                className={styles.bannerImage}
-                fill
-                sizes="(max-width: 768px) 100vw, 200px"
-                style={{ objectFit: "cover" }}
-              />
-              <div className={styles.bannerOverlay}>
-                <h3 className={styles.bannerTitle}>Скидки до 50%</h3>
-                <p className={styles.bannerText}>По всей России</p>
-              </div>
-            </div>
-          </div>
-
           {/* <ScrollIndicator /> */}
 
           <VipNumberModal isOpen={isModalOpen} onClose={closeModal} />
